acb662af333912038dd59d8d4d7e19e6d7438999
Eric ONDOA <eric.ondoa@gmail.com>
2019-05-09 12:08:47 +0200

    feat(Authentification): add authentification Service.

---
+++ src/app/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+interface myData {
+  success: boolean,
+  message: string}
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private loggedInStatus = false;
+
+  constructor(private http: HttpClient) { }
+
+  setLoggedIn(value: boolean) {
+    this.loggedInStatus = value;
+  }
+
+  get isLoggedIn() {
+    return this.loggedInStatus;
+  }
+  getUserDetails(user, password) {
+    // Post these details to API server return user info
+    return this.http.post<myData>('/api/auth', {
+      user,
+      password
+    });
+  }
+}
