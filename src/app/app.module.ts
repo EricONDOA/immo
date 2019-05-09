08cf6464f6f269bac24405790a572fbf4b21b432
Eric ONDOA <eric.ondoa@gmail.com>
2019-05-09 12:30:48 +0200

    feat(AppModule): remove register component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AuthService } from './auth.service';
+import { UserService } from './user.service';
+import { AuthGuard } from './auth.guard';
+import { LogoutComponent } from './logout/logout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ClientResolverService } from './client-resolver.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    LogoutComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: HomeComponent
+      },
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: 'logout', component: LogoutComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        resolve: { resolvedData: ClientResolverService }
+      }
+    ])
   ],
-  providers: [],
+  providers: [AuthService, UserService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
